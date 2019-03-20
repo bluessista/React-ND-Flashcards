080c9f2568750c80449a1626431360b62952660c
bluessista <inga@bluessista.com>
2019-03-20 22:24:55 +0100

    feat: complete App with notification

--- App.js
+++ App.js
@@ -1,24 +1,108 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import { white, green, orange } from './utils/colors';
+import { setDailyNotification } from './utils/helpers';
 
 // components
 import DeckList from './components/DeckList';
+import AddDeck from './components/AddDeck';
+import AddQuestion from './components/AddQuestion';
+import DeckView from './components/DeckView';
+import Quiz from './components/Quiz';
+import { gray } from 'ansi-colors';
+
+const Tabs = createBottomTabNavigator({
+    Home : {
+        screen: DeckList,
+        navigationOptions: {
+            tabBarLabel: 'Home'
+        },
+    },
+    AddDeck: {
+        screen: AddDeck,
+        navigationOptions: {
+            tabBarLabel: 'Add Deck'
+        },
+    }
+  }, {
+    tabBarOptions: {
+        activeTintColor: green,
+        inactiveBackgroundColor: gray,
+        labelStyle: {
+            fontSize: 20,
+        },
+        style: {
+            height: 56,
+            backgroundColor: white
+        }
+    }
+});
+
+const MainNavigator = createStackNavigator({
+    Home: {
+        screen: Tabs,
+        navigationOptions: {
+            header: null
+        }
+    },
+    AddQuestion: {
+        screen: AddQuestion,
+        navigationOptions: {
+            title: 'Add Question',
+            headerTintColor: white,
+            headerTitleSize: {
+                fontSize: 20
+            },
+            headerStyle: {
+                backgroundColor: 'black',
+            }
+        }
+    },
+    Deck: {
+        screen: DeckView,
+        navigationOptions: {
+            title: 'Deck Details',
+            headerTintColor: white,
+            headerTitleSize: {
+                fontSize: 20
+            },
+            headerStyle: {
+                backgroundColor: 'black',
+            }
+        }
+    },
+    Quiz: {
+        screen: Quiz,
+        navigationOptions: {
+            title: 'Quiz',
+            headerTintColor: white,
+            headerTitleSize: {
+            fontSize: 20
+            },
+            headerStyle: {
+                backgroundColor: 'black',
+            }
+        }
+    }
+},
+{
+  initialRouteName: "Home"
+}
+);
+
+const AppContainer = createAppContainer(MainNavigator);
 
 export default class App extends Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <DeckList />
-      </SafeAreaView>
-    );
-  }
-}
+    componentDidMount() {
+        setDailyNotification();
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    render() {
+        return (
+        <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+            <AppContainer />
+        </SafeAreaView>
+        );
+    }
+}
